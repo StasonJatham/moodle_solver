1809f4912c75d41c64dbbe303ae8f18c6fe8849d
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2023-04-30 01:15:45 +0200

    edit: get up to snuff for moodle v3.9

--- get_answers_german.js
+++ get_answers_german.js
@@ -1,35 +1,71 @@
 // -> moodle tests have 2 tries, on the first try just random answer everything
 // -> go to review test once done / or just wait till you re on the review page, this saves it to localstorage
 // -> on the review page run this script, for portabillity transform this into a bookmarklet.
-// -> ONLY WORKS ON GERMAN MOODLE CHANGE LINES: 13 and 22
+// -> ONLY WORKS ON GERMAN MOODLE, but which is easily adaptable
+// supported question types:
+// - multiple-choice, single answer, regular
+// - multiple-choice, single answer, true/false
+// - multiple-choice, multiple answers
+// - multiple selects
 
 (function () {
   function parseCorrectAnswers() {
-    var results = [];
+    const results = [];
     $(".que").each(function () {
-      var text = $(".qtext", this).text().replace(/(\s+)/gm, "");
-      var answer = $(".rightanswer", this).text();
-      if (answer.startsWith("Die richtige Antwort lautet: ")) {
-        answer = answer.replace("Die richtige Antwort lautet: ", "");
-        answer = answer.replace(/(\s+)/gm, "");
-      } else {
-        if ($("span", $(".rightanswer", this)).length > 1) {
-          answer = [];
-          $("span", $(".rightanswer", this)).each(function () {
-            answer.push($(this).text().replace(/(\s+)/gm, ""));
-          });
-        } else {
-          answer = answer.replace("Die richtigen Antworten sind: ", "");
-          answer = answer.split(", ");
-          for (var x = 0; x < answer.length; x++) {
-            answer[x] = answer[x].replace(/(\s+)/gm, "");
+      const text = $(".qtext", this).text().replace(/(\s+)/gm, "");
+      const answer = $(".rightanswer", this).text();
+      var parsedAnswer;
+
+      if (
+        (answer.startsWith("Die richtige Antwort ist: ") && !answer.includes(" → ")) ||
+        (answer.startsWith("Die richtige Antwort ist '") && answer.endsWith("'."))
+      ) {
+        // multiple-choice single answer
+        parsedAnswer = answer
+          .replace("Die richtige Antwort ist: ", "")
+          .replace("Die richtige Antwort ist '", "")
+          .replace("'.", "")
+          .replace(/(\s+)/gm, "");
+      } else if (answer.startsWith("Die richtigen Antworten sind: ")) {
+        // multiple-choice multiple answers
+        parsedAnswer = [];
+        var simpleSplit = answer.replace("Die richtigen Antworten sind: ", "").split(", ");
+        // handle spans
+        if ($(".rightanswer span", this).length > 0) {
+          var answersHtml = $(".rightanswer", this).html().replace("Die richtigen Antworten sind: ", "").replace(/<\/span[^>]*><span[^>]*>/g, "").replace(/\r?\n|\r/g, " ");
+          const regex = /<span[^>]*>.*?<\/span[^>]*>/g
+          var match;
+          while ((match = regex.exec(answersHtml)) !== null) {
+            // add parsed textual content as answer
+            parsedAnswer.push(new DOMParser().parseFromString(match[0], 'text/html').querySelector('span').textContent.replace(/(\s+)/gm, ''));
+          }
+
+          // remove span html stuff to only keep elements simply delimited by comma
+          answersHtml = answersHtml.replace(/(<\/span[^>]*>)\s*,\s*(<span[^>]*>)/g, "$1$2").replace(regex, '');
+          simpleSplit = new DOMParser().parseFromString(answersHtml, 'text/html').querySelector('body').textContent.split(", ");
+        }
+        for (var x = 0; x < simpleSplit.length; x++) {
+          const curItem = simpleSplit[x].replace(/(\s+)/gm, '');
+          if(curItem.length > 0) {
+            parsedAnswer.push(curItem);
           }
         }
+      } else if (answer.startsWith("Die richtige Antwort ist: ") && answer.includes(" → ")) {
+        // multiple selects
+        parsedAnswer = answer.replace("Die richtige Antwort ist: ", "");
+      } else {
+        alert("Unable to retrieve question-answer pair for question: " + text);
+        throw new Error("Unable to retrieve question-answer pair for question: " + text);
+        return;
       }
-      results.push({ question: text, answer: answer });
+
+      results.push({question: text, answer: parsedAnswer});
     });
+
     localStorage.setItem("answers", JSON.stringify(results));
+    console.log(results);
     alert("Parsing complete!");
   }
+
   parseCorrectAnswers();
 })();
--- set_answers.js
+++ set_answers.js
@@ -1,52 +1,92 @@
 // uses the question answer pairs from get_answers.js from localstorage
 // fills right answer and submits to next question
+// supported question types:
+// - multiple-choice, single answer, regular
+// - multiple-choice, single answer, true/false
+// - multiple-choice, multiple answers
+// - multiple selects (no auto-fill, only output of answers)
 
 (function () {
   function setCorrectAnswers() {
     var results = JSON.parse(localStorage.getItem("answers"));
-    $(".que").each(function () {
-      var question = $(".qtext", this).text().replace(/(\s+)/gm, "");
+    var questionElement = document.querySelector(".qtext");
+    if (questionElement) {
+      var question = questionElement.textContent.replace(/(\s+)/gm, "");
       var answers = null;
       results.forEach(function (value, key) {
-        console.log(key, value, "questions");
         if (value.question === question) {
           answers = value.answer;
         }
       });
+      console.log("Question: '" + question + "'");
+      console.log("Answers: ");
+      console.log(answers);
       if (answers === null) {
         alert("Question not found!");
         return;
       }
-      var answered = false;
-      $("label.ml-1").each(function () {
-        var num = $(".answernumber", this).text().replace(/(\s+)/gm, "");
-        var text = $(this).text().replace(/(\s+)/gm, "");
-        if (num) {
-          text = text.replace(num, "").trim();
-        }
-        var id = $(this).attr("for");
-        var correct = false;
-        if (typeof answers === "string") {
-          if (text === answers.replace(/(\s+)/gm, "")) {
-            correct = true;
+      var answerTable = document.querySelector('table.answer');
+      var answerLabels = document.querySelectorAll('[data-region="answer-label"]');
+      var answerRadioInput = document.querySelector('input[type=radio]');
+      if (answerTable !== null) {
+        // multiple select
+        alert("Automatic solving of multi-selects isn't supported yet, but the answers were printed to the console :)");
+      } else if (answerLabels.length > 0) {
+        // multiple choice
+        answerLabels.forEach(function (label) {
+          var num = label.querySelector(".answernumber").textContent.replace(/(\s+)/gm, "");
+          var text = label.textContent.replace(/(\s+)/gm, "");
+          if (num) {
+            text = text.replace(num, "").trim();
           }
-        } else {
-          answers.forEach(function (value, key) {
-            if (text === value.replace(/(\s+)/gm, "")) {
+          var id = label.id.replace("_label", "");
+          var correct = false;
+          if (typeof answers === "string") {
+            // single answer
+            if (text === answers.replace(/(\s+)/gm, "")) {
               correct = true;
             }
-          });
+          } else if (Array.isArray(answers)) {
+            // multiple answers
+            answers.forEach(function (value, key) {
+              if (text === value.replace(/(\s+)/gm, "")) {
+                correct = true;
+                const index = answers.indexOf(value.replace(/(\s+)/gm));
+                if (index > -1) {
+                  answers.splice(index, 1);
+                }
+              }
+            });
+          }
+          var inputElement = document.getElementById(id);
+          if (inputElement) {
+            inputElement.checked = correct;
+          }
+        });
+        if (Array.isArray(answers) && answers.length !== 0) {
+          alert("There are some answers that weren't found: " + answers);
+        } else {
+          $(".mod_quiz-next-nav").trigger("click");
         }
-        if (correct) {
-          document.getElementById(id).checked = true;
-          answered = true;
+      } else if (answerRadioInput !== null) {
+        // multiple choice - single answer - true/false
+        var answerId = answerRadioInput.name;
+        if (answers.replace(/(\s+)/gm, "") === "Wahr") {
+          document.getElementById(answerId + "true").checked = true
+          $(".mod_quiz-next-nav").trigger("click");
+        } else if (answers.replace(/(\s+)/gm, "") === "Falsch") {
+          document.getElementById(answerId + "false").checked = true;
+          $(".mod_quiz-next-nav").trigger("click");
+        } else {
+          alert("Expected the answer to be 'Wahr' or 'Falsch', but is: " + answers);
         }
-      });
-      if (!answered) {
-        alert("Answer wasnt found!");
+      } else {
+        alert("Unknown question type!");
       }
-    });
-    $(".mod_quiz-next-nav").trigger("click");
+    } else {
+      alert("Question element not found!");
+    }
   }
+
   setCorrectAnswers();
 })();
